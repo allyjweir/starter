7caca6ae6cca59b253ddf11762566bf8c9b94cfe
Ally Weir <ally.weir@spire.com>
2021-07-22 18:12:12 +0100

    Add example custom Prometheus metric

--- app.py
+++ app.py
@@ -1,15 +1,27 @@
 from starlette.applications import Starlette
 from starlette.responses import JSONResponse
+from starlette.middleware import Middleware
 from starlette.routing import Route
+from starlette_prometheus import metrics, PrometheusMiddleware
+from prometheus_client import Counter
+
+greetings_counter = Counter(
+    "greetings_given", "the number of times we have said hello on the homepage"
+)
 
 
 async def homepage(request):
+    greetings_counter.inc()
     return JSONResponse({"hello": "dan"})
 
 
 starter = Starlette(
     debug=True,
+    middleware=[
+        Middleware(PrometheusMiddleware),
+    ],
     routes=[
         Route("/", homepage),
+        Route("/metrics/", metrics),
     ],
 )
