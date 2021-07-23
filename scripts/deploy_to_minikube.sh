3f93c176039fcfa48161ac9dceff157ee6225e56
Ally Weir <ally.weir@spire.com>
2021-07-23 16:47:30 +0100

    Improve script output

--- scripts/deploy_to_minikube.sh
+++ scripts/deploy_to_minikube.sh
@@ -2,14 +2,30 @@
 
 set -eu
 
+if [ "$(kubectl config current-context)" != "minikube" ]; then
+    echo "This script expects minikube to be running and set as the current kubectl context."
+    exit 1
+fi
+
+echo "Ensuring minikube's ingress addon is configured"
+echo "======"
+minikube addons enable ingress
+
+echo
+echo "Verifying kapp is installed"
+echo "======"
 if ! kapp --version; then
     echo "kapp is used to deploy to minikube."
     echo "Please install here: https://carvel.dev/kapp/"
     exit 1
 fi
 
-minikube addons enable ingress
-
+echo
+echo "Deploying app via kapp"
+echo "======"
 kapp deploy --yes --app starter-app --file deployment/manifest.yaml
 
-echo "If you haven't already, make sure to configure your minikube nginx ingress in /etc/hosts as specified here: https://kubernetes.io/docs/tasks/access-application-cluster/ingress-minikube/#create-an-ingress-resource"
+echo
+echo "NOTES"
+echo "======"
+echo "1) Make sure to configure your minikube nginx ingress in /etc/hosts as specified here: https://kubernetes.io/docs/tasks/access-application-cluster/ingress-minikube/#create-an-ingress-resource"
